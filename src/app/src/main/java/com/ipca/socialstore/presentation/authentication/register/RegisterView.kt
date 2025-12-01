7a2c85bb4549dcf523212b163b2986d8f007823d
CrZ <david123car7@gmail.com>
2025-12-01 17:45:03 +0000

    Redo Home Views and Routes

--- src/app/src/main/java/com/ipca/socialstore/presentation/authentication/register/RegisterView.kt
+++ src/app/src/main/java/com/ipca/socialstore/presentation/authentication/register/RegisterView.kt
@@ -22,8 +22,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
-import com.ipca.socialstore.presentation.objects.NavigationViews
-import com.ipca.socialstore.presentation.utils.ErrorText
+import com.ipca.socialstore.presentation.objects.Routes
 import com.ipca.socialstore.ui.theme.SocialStoreTheme
 
 
@@ -45,7 +44,7 @@ fun RegisterView(modifier: Modifier, navController: NavController){
 
     LaunchedEffect(uiState.isRegistered) {
         if(uiState.isRegistered)
-            navController.navigate(NavigationViews.login)
+            navController.navigate(Routes.Login)
     }
 }
 
---
+++ src/app/src/main/java/com/ipca/socialstore/presentation/objects/Routes.kt
@@ -0,0 +1,36 @@
+package com.ipca.socialstore.presentation.objects
+
+import kotlinx.serialization.Serializable
+
+sealed class Routes {
+
+    // --- Authentication ---
+    @Serializable
+    object Login
+
+    @Serializable
+    object Register
+
+    @Serializable
+    object RequestResetPassword
+
+    @Serializable
+    object ResetPassword
+
+    // --- Home Views ---
+    @Serializable
+    object DefaultHome
+
+    @Serializable
+    object BeneficiaryHome
+
+    @Serializable
+    object AdminHome
+
+    // --- Campaigns ---
+    @Serializable
+    object CreateCampaign
+
+    @Serializable
+    object ListAllCampaign
+}
